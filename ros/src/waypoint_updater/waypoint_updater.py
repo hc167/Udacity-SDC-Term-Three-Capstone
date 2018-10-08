c2964d6e0c562f5314c197953e3381b610b4fd17
redpanda3 <yuda@berkeley.edu>
2018-10-08 01:06:38 -0700

    remove current velocity initialization

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -33,7 +33,7 @@ class WaypointUpdater(object):
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
         rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
-        rospy.Subscriber('/current_velocity', TwistStamped, self.current_velocity_cb)
+
 
         #Publisher
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
@@ -47,7 +47,6 @@ class WaypointUpdater(object):
         #self.stopline_wp_idx = 200
         self.waypoint_tree = None
         self.waypoints_2d = None
-        self.current_velocity = 0.0
 
 
         ##loop and publish waypoints
